c5e927100a3fd1d30269677f540b0b2924de171f
9007967 <33853639+9007967@users.noreply.github.com>
2020-03-30 21:58:01 +0800

    Update config.php
    新增自定义插件文件夹和数据库获取钩子列表

--- src/config.php
+++ src/config.php
@@ -2,8 +2,14 @@
 
 return [
     'autoload' => false,
+    'database' => [ //数据表获取钩子
+        'expire'  => 0 //查询缓存时间（单位秒，0为不缓存）
+        , 'cache' => '__hooks_data_cache__' // 钩子数据缓存标识
+        , 'table' => 'hooks' //钩子数据存放表名称
+        , 'field' => [ 'mark', 'list' ] //钩子数据读取字段 （mark = 钩子标识，list = 使用钩子的插件列表
+    ],
     'hooks'    => [],
     'route'    => [],
     'service'  => [],
-    'dir'      => 'addons'
+    'dir'      => 'addons' //自定义插件文件夹
 ];
